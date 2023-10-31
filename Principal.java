071188d2f68341d88b1cacbc367fb748291e51d5
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-10-31 13:49:49 +0000

    salvando

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        Processo proc1 = new Processo("Leo",17);
+        Processo proc2 = new Processo("Leo",17);
+
+        
+        //objetos proc1 e proc2
+        //start
+    }
+    
+}
